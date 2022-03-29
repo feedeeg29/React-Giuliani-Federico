3e04489f117afdcf81dcab86f7d29593721ed37b
Federico Giuliani <giuliani.federic0@gmail.com>
2022-03-29 20:33:41 -0300

    seguunda entrga

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import logo from './logo.svg';
 import './App.css';
 import MyNavBar from "./Components/MyNavBar";
 
@@ -7,6 +8,7 @@ export default function App() {
   return (
     <>
     <MyNavBar />
+    <h1>Acá van más cosas</h1>
     </>
     )
 }
